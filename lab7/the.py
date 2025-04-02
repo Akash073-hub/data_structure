000cda81661abd6b6b529321c050f36407f3b813
Akash073-hub <akashramachandrabhatbca24@rvu.edu.in>
2025-04-02 10:53:07 +0530

    Add files via upload

--- lab7/the.py
+++ lab7/the.py
@@ -1,1 +1,39 @@
+class Node: 
+    def __init__(self, data): 
+        self.data = data 
+        self.next = None 
+
+class Queue: 
+    def __init__(self): 
+        self.front = None 
+        self.rear = None 
+    
+    def enqueue(self, data):
+        new_node = Node(data)
+        if self.rear is None:
+            self.front = self.rear = new_node
+            return
+        else:
+            self.rear.next = new_node
+            self.rear = new_node
+    
+    def dequeue(self):
+        if self.front is None:
+            print("Queue is empty")
+        else:
+            temp = self.front
+            self.front = temp.next
+            print(f"Dequeued: {temp.data}")
+            if self.front is None:
+                self.rear = None 
+
+
+q = Queue()
+q.enqueue(10)
+q.enqueue(20)
+q.enqueue(30)
+q.dequeue()  
+q.dequeue() 
+q.dequeue() 
+q.dequeue() 
 
